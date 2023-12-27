4e370b46e0267ae7f84133413f06896ad7985c49
mrlonely <116348059+mameikagou@users.noreply.github.com>
2023-12-27 21:14:57 +0800

    up

--- 03-pizza-menu/starter/react/src/App.jsx
+++ 03-pizza-menu/starter/react/src/App.jsx
@@ -1,69 +1,104 @@
 import { useState, useEffect } from "react";
 
-import "./App.css";
+// import "./App.css";
 import spinaci from "#/pizzas/spinaci.jpg";
 import React from "react";
+import "#/index.css";
+
+
+// 因为使用纯函数, 所以react的变量是不可变的; 
+
+// react还使用单向数据流, 一般从父组件到子组件, 而不能反过来; 
+
+// 相对于angular的双向; 
+
 
 function App() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 20;
-  (hour > openHour && hour < closeHour)
-    ? console.log(`Sorry, we are closed now. Open daily from ${openHour}:00 to ${closeHour}:00`)
-    : console.log(`We are open now. Open daily from ${openHour} to ${closeHour}`);
+  hour > openHour && hour < closeHour
+    ? console.log(
+        `Sorry, we are closed now. Open daily from ${openHour}:00 to ${closeHour}:00`
+      )
+    : console.log(
+        `We are open now. Open daily from ${openHour} to ${closeHour}`
+      );
 
   return (
-    <>
+    <div className="container">
       <Header></Header>
       <Menu></Menu>
       <Footer></Footer>
-    </>
+    </div>
   );
 }
 
 function Header() {
+  const HeaderStyle = {};
+
   return (
-    <>
-      <h2>Fast React Pizza Co.</h2>
-    </>
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
   );
 }
 function Menu() {
   return (
-    <>
-      <h3>Menu</h3>
-      <Pizza></Pizza>
-      <Pizza></Pizza>
-      <Pizza></Pizza>
-    </>
+    <div className="menu">
+      <h2>our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Spinach, mozzarella, tomato sauce, garlic, olive oil"
+        photoName={spinaci}
+        price="12.50"
+      ></Pizza>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Spinach, mozzarella, tomato sauce, garlic, olive oil"
+        photoName={spinaci}
+        price="12.50"
+      ></Pizza>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Spinach, mozzarella, tomato sauce, garlic, olive oil"
+        photoName={spinaci}
+        price="12.50"
+      ></Pizza>
+    </div>
   );
 }
 function Footer() {
   const [time, setTime] = useState(() => new Date().toLocaleTimeString); // 这里居然是个函数调用吗
-  
+
   // useEffect好像会导致这个组件一直刷新; 导致旁边的东西, 一直在闪烁
-  useEffect(() => {
-    const timeId = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => clearInterval(timeId);
-  }, []// 空依赖项数组表示该效果仅在组件挂载时运行一次
+  useEffect(
+    () => {
+      const timeId = setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+      }, 1000);
+      return () => clearInterval(timeId);
+    },
+    [] // 空依赖项数组表示该效果仅在组件挂载时运行一次
   );
 
   return React.createElement(
     "footer",
-    null,
+    { className: "footer" },
     `${time}  &copyright; 2021 Fast React Pizza Co.`
   );
 }
 
-function Pizza() {
+function Pizza({ name, ingredients, photoName, price }) {
   return (
-    <>
-      <img src={spinaci}></img>
-      <h4>Pizza Spinaci</h4>
-      <p>Spinach, mozzarella, tomato sauce, garlic, olive oil</p>
-    </>
+    <div className="pizza">
+      <img src={photoName} alt={name}></img>
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}</span>
+      </div>
+    </div>
   );
 }
 export default App;
